0153111bfaca5ac072707f00790c62965a92ba96
Connor Rothschild <connorrothschild@gmail.com>
2022-11-28 20:46:10 -0600

    🎉  See you at the workshop

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:connorrothschild/svelte-d3-iib-workshop.git main:gh-pages
+git push -f git@github.com:connorrothschild/iib-svelte-workshop-chart.git main:gh-pages
 
 cd -
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/svelte-d3-iib-workshop/'
+  base: '/iib-svelte-workshop-chart/'
 })
